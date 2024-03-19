c078f4875e4c67b94af44514ac263a839ea60ce3
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-19 12:19:49 +0800

    koala method complete

--- hw02/HW02_4111056036_4.java
+++ hw02/HW02_4111056036_4.java
@@ -1,129 +1,119 @@
 public class HW02_4111056036_4 extends FourSum {
-    public HW02_4111056036_4() {
+    Position[] sums;
 
+    public HW02_4111056036_4() {
+        
     }
 
-    public void quickSort(int[] arr, int low, int high) {
-        int pi;
+    public void quickSort(Position[] arr, int low, int high) {
         if (low < high) {
-            pi = partition(arr, low, high);
+            int pi = partition(arr, low, high);
 
             quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
     }
 
-    private int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
+    private int partition(Position[] arr, int low, int high) {
+        int pivot = arr[high].value;
         int i = (low - 1);
-        int temp;
         for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
+            if (arr[j].value < pivot) {
                 i++;
 
-                temp = arr[i];
+                Position temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
 
-        temp = arr[i + 1];
+        Position temp = arr[i + 1];
         arr[i + 1] = arr[high];
         arr[high] = temp;
 
         return i + 1;
     }
 
-    private static class SplitCheck extends Thread {
-        private int[] A;
-        private int from;
-        private int to;
-        private int last;
-        private int result;
+    private class Position {
+        int value;
+        int id_1;
+        int id_2;
 
-        public SplitCheck(int[] A, int from, int to, int last) {
-            this.A = A;
-            this.from = from;
-            this.to = to;
-            this.last = last;
-        }
-
-        @Override
-        public void run() {
-            int ans = 0;
-            int start, end, sum, low, high;
-            for(int i = this.from; i < this.to; i++) {
-                if(i > 0 && this.A[i] == this.A[i - 1]) {
-                    continue;
-                }
-    
-                for(int j = i + 1; j < this.last; j++) {
-                    if(j > (i + 1) && this.A[j] == this.A[j - 1]) {
-                        continue;
-                    }
-    
-                    start = j + 1;
-                    end = this.last - 1;
-                    while(start < end) {
-                        sum = this.A[i] + this.A[j] + this.A[start] + this.A[end];
-                        if(sum == 0) {
-                            ans++;
-                            low = this.A[start];
-                            high = this.A[end];
-                            start++;
-                            end--;
-                            while(start < end && this.A[start] == low) {
-                                start++;
-                                ans++;
-                            }
-                            while(start < end && this.A[end] == high) {
-                                end--;
-                                ans++;
-                            }
-                        } else if(sum > 0) {
-                            end--;
-                        } else {
-                            start++;
-                        }
-                    }
-                }
-            }
-
-            this.result = ans;
-        }
-
-        public int getResult() {
-            return this.result;
+        Position(int value, int id_1, int id_2) {
+            this.value = value;
+            this.id_1 = id_1;
+            this.id_2 = id_2;
         }
     }
 
     @Override
     public int F_sum(int[] A) {
         int n = A.length;
+        sums = new Position[n * (n - 1) / 2];
+        int now = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                sums[now] = new Position(A[i] + A[j], i, j);
+                now++;
+            }
+        }
+
+        this.quickSort(sums, 0, sums.length - 1);
+
+        int left = 0;
+        int right = sums.length - 1;
         int ans = 0;
-        this.quickSort(A, 0, n - 1);
+        int[] record = new int[sums.length];
+        int leftCount, rightCount, low, high, initLeft, initRight;
+        while(left < right) {
+            if(sums[left].value + sums[right].value == 0) {
+                if(sums[left].value == 0) {
+                    for(int i = left; i <= right; i++) {
+                        for(int j = i + 1; j <= right; j++) {
+                            if(sums[i].id_1 != sums[j].id_1 && sums[i].id_1 != sums[j].id_2 && sums[i].id_2 != sums[j].id_1 && sums[i].id_2 != sums[j].id_2) {
+                                ans++;
+                            }
+                        }
+                    }
+                    break;
+                }
 
-        int threadNums = Runtime.getRuntime().availableProcessors();
-        int amountOnce = n / threadNums;
+                leftCount = 0;
+                rightCount = 0;
+                low = sums[left].value;
+                high = sums[right].value;
 
-        SplitCheck[] workQueue = new SplitCheck[threadNums];
+                initLeft = left;
+                initRight = right;
 
-        for(int i = 0; i < threadNums - 1; i++) {
-            workQueue[i] = new SplitCheck(A, amountOnce * i, amountOnce * (i + 1), n);
-            workQueue[i].start();
+                while(left < right && sums[left].value == low) {
+                    record[sums[left].id_1]++;
+                    record[sums[left].id_2]++;
+                    leftCount++;
+                    left++;
+                }
+
+                while(left <= right && sums[right].value == high) {
+                    ans -= record[sums[right].id_1];
+                    ans -= record[sums[right].id_2];
+                    rightCount++;
+                    right--;
+                }
+
+                while(initLeft < initRight && sums[initLeft].value == low) {
+                    record[sums[initLeft].id_1]--;
+                    record[sums[initLeft].id_2]--;
+                    initLeft++;
+                }
+
+                ans += leftCount * rightCount;
+            } else if(sums[left].value + sums[right].value > 0) {
+                right--;
+            } else {
+                left++;
+            }
         }
 
-        workQueue[threadNums - 1] = new SplitCheck(A, amountOnce * (threadNums - 1), n, n);
-        workQueue[threadNums - 1].start();
-
-        for(int i = 0; i < threadNums; i++) {
-            try {
-                workQueue[i].join();
-            } catch (InterruptedException e) { }
-            
-            ans += workQueue[i].getResult();
-        }
-        
-        return ans;
+        return ans / 3;
     }
 }
