6c21a57aa73ae4ef7100a622e519806169011b3c
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-19 16:12:12 +0800

    qsort multithread

--- hw02/HW02_4111056036_4.java
+++ hw02/HW02_4111056036_4.java
@@ -79,6 +79,99 @@ public class HW02_4111056036_4 extends FourSum {
         }
     }
 
+    class QuickSortThread extends Thread {
+        private static int threadCount = 0;
+        private static final int MAX_THREADS = Runtime.getRuntime().availableProcessors();
+        private static final int THRESHOLD = 10;
+    
+        private Position[] arr;
+        private int low, high;
+    
+        public QuickSortThread(Position[] arr, int low, int high) {
+            this.arr = arr;
+            this.low = low;
+            this.high = high;
+        }
+    
+        @Override
+        public void run() {
+            quickSort(arr, low, high);
+        }
+    
+        private void quickSort(Position[] arr, int low, int high) {
+            if (high - low < THRESHOLD) {
+                insertionSort(arr, low, high);
+            } else if (low < high) {
+                int pi = partition(arr, low, high);
+    
+                boolean createNewThreads = false;
+                synchronized (QuickSortThread.class) {
+                    if (threadCount < MAX_THREADS - 1) {
+                        threadCount += 2;
+                        createNewThreads = true;
+                    }
+                }
+    
+                if (createNewThreads) {
+                    QuickSortThread leftSort = new QuickSortThread(arr, low, pi - 1);
+                    QuickSortThread rightSort = new QuickSortThread(arr, pi + 1, high);
+    
+                    leftSort.start();
+                    rightSort.start();
+    
+                    try {
+                        leftSort.join();
+                        rightSort.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+    
+                    synchronized (QuickSortThread.class) {
+                        threadCount -= 2;
+                    }
+                } else {
+                    quickSort(arr, low, pi - 1);
+                    quickSort(arr, pi + 1, high);
+                }
+            }
+        }
+    
+        private void insertionSort(Position[] arr, int low, int high) {
+            for (int i = low + 1; i <= high; i++) {
+                Position key = arr[i];
+                int j = i - 1;
+    
+                while (j >= low && arr[j].value > key.value) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    
+        private int partition(Position[] arr, int low, int high) {
+            int pivot = arr[high].value;
+            int i = (low - 1);
+            for (int j = low; j < high; j++) {
+                if (arr[j].value < pivot) {
+                    i++;
+    
+                    if (i != j) {
+                        Position temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+    
+            Position temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+    
+            return i + 1;
+        }
+    }
+
     @Override
     public int F_sum(int[] A) {
         this.A = A;
@@ -105,7 +198,11 @@ public class HW02_4111056036_4 extends FourSum {
             } catch (InterruptedException e) { }
         }
 
-        this.quickSort(this.sums, 0, this.sums.length - 1);
+        QuickSortThread sortThread = new QuickSortThread(this.sums, 0, this.sums.length - 1);
+        sortThread.start();
+        try {
+            sortThread.join();
+        } catch (InterruptedException e) { }
 
         int left = 0;
         int right = sums.length - 1;
