8d51c771a270d62449ed1eb4abb387fe82a8b547
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-23 01:02:06 +0800

    update hw02 build 4 and build 5

--- hw02/HW02_4111056036_5.java
+++ hw02/HW02_4111056036_5.java
@@ -72,33 +72,6 @@ public class HW02_4111056036_5 extends FourSum {
         }
     }
     
-    class SplitPut extends Thread {
-        int from, to, last;
-        SumTable table;
-        int[] A;
-        
-        public SplitPut(int from, int to, int last, SumTable table, int[] A) {
-            this.from = from;
-            this.to = to;
-            this.last = last;
-            this.table = table;
-            this.A = A;
-        }
-    
-        @Override
-        public void run() {
-            int sum;
-            for(int i = this.from; i < this.to; ++i) {
-                for(int j = i + 1; j < this.last; ++j) {
-                    sum = A[i] + A[j];
-                    if(sum <= 0) {
-                        table.put(sum, j);
-                    }
-                }
-            }
-        }
-    }
-    
     class SplitCount extends Thread {
         int from, to, last, result;
         SumTable table;
@@ -137,27 +110,24 @@ public class HW02_4111056036_5 extends FourSum {
         int count = 0;
         int n = A.length;
 
-        SumTable table = new SumTable(1000000);
+        SumTable table = new SumTable(25000);
 
         java.util.Arrays.sort(A);
+
+        int sum;
+        for (int i = 0; i < A.length; ++i) {
+            for (int j = i + 1; j < A.length; ++j) {
+                sum = A[i] + A[j];
+                if (sum <= 0) {
+                    table.put(sum, j);
+                }
+            }
+        }
 
         int threadNums = Runtime.getRuntime().availableProcessors();
         int amountOnce = n / threadNums;
 
-        SplitPut[] workQueue = new SplitPut[threadNums];
         SplitCount[] workQueue2 = new SplitCount[threadNums];
-
-        for(int i = 0; i < threadNums - 1; i++) {
-            workQueue[i] = new SplitPut(amountOnce * i, amountOnce * (i + 1), n, table, A);
-            workQueue[i].start();
-        }
-        workQueue[threadNums - 1] = new SplitPut(amountOnce * (threadNums - 1), n - 1, n, table, A);
-        workQueue[threadNums - 1].start();
-        for(int i = 0; i < threadNums; i++) {
-            try {
-                workQueue[i].join();
-            } catch (InterruptedException e) { }
-        }
 
         for(int i = 0; i < threadNums - 1; i++) {
             workQueue2[i] = new SplitCount(amountOnce * i, amountOnce * (i + 1), n, table, A);
