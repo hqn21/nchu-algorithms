9ea88a27c5d2ce6fa9daff419a2f51e79e27066e
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-24 00:43:50 +0800

    hw02 build 4 AC

--- hw02/HW02_4111056036_4.java
+++ hw02/HW02_4111056036_4.java
@@ -1,147 +1,194 @@
-public class HW02_4111056036_4 extends FourSum {
-    class TableNode {
-        int position, value;
-        TableNode next;
-    
-        public TableNode(int position) {
-            this.position = position;
-            this.value = 0;
-            this.next = null;
-        }
+class HashMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public HashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
     }
-    
-    class SumTable {
-        int tableSize;
-        TableNode[] table;
-    
-        public SumTable(int tableSize) {
-            this.table = new TableNode[this.tableSize = tableSize];
-        }
-    
-        public int hash(int key) {
-            int hashKey = ((key % tableSize) + tableSize) % tableSize;
-            while (table[hashKey] != null && table[hashKey].position != key) {
-                hashKey = (hashKey + 1) % tableSize;
-            }
-            return hashKey;
-        }
-    
-        public void put(int sum, int endPosition) {
-            TableNode head = table[hash(sum)];
-            if (head == null) {
-                head = table[hash(sum)] = new TableNode(sum);
-            }
-    
-            head.value++;
-    
-            for (TableNode node = head; ; node = node.next) {
-                if (node != head && node.position == endPosition) {
-                    node.value++;
-                    return;
-                } else if (node.next == null || node.next.position > endPosition) {
-                    TableNode newNode = new TableNode(endPosition);
-                    newNode.next = node.next;
-                    node.next = newNode;
-                    newNode.value++;
-                    return;
+
+    @SuppressWarnings("unchecked")
+    private void grew() {
+        if ((size + 1) == (table.length * DEFAULT_LOAD_FACTOR)) {
+            Node<K, V>[] oldTable = table;
+            table = new Node[table.length << 1];
+            size = 0;
+            for (int i = 0; i < oldTable.length; i++) {
+                Node<K, V> node = oldTable[i];
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
                 }
             }
         }
-    
-        public int count(int sum, int startPosition) {
-            TableNode head = table[hash(sum)];
-    
-            if (head == null) {
-                return 0;
-            }
-    
-            int acc = 0;
-    
-            for (TableNode node = head.next; node != null; node = node.next) {
-                if (node.position >= startPosition) {
+    }
+
+    public void put(K key, V value) {
+        grew();
+
+        int hash = hash(key);
+        int index = calcIndex(hash);
+        Node<K, V> node = table[index];
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        if (node == null) {
+            table[index] = newNode;
+        } else {
+            boolean keyRepeat = false;
+            Node<K, V> last = null;
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    keyRepeat = true;
+                    node.value = value;
                     break;
+                } else {
+                    last = node;
+                    node = node.next;
                 }
-                acc += node.value;
             }
-    
-            return acc;
+            if (!keyRepeat) {
+                last.next = newNode;
+            }
         }
-    
-        public boolean containsKey(int key) {
-            return table[hash(key)] != null;
+
+        size++;
+    }
+
+    public V get(K key) {
+        int hash = hash(key);
+        int index = calcIndex(hash);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            } else {
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int hash = hash(key);
+        int index = calcIndex(hash);
+        Node<K, V> node = table[index];
+        if (node != null && node.key.equals(key)) {
+            Node<K, V> next = node.next;
+            table[index] = next;
+            size--;
+            return node.value;
+        } else {
+            Node<K, V> pre = node;
+            node = node.next;
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    Node<K, V> next = node.next;
+                    pre.next = next;
+                    size--;
+                    return node.value;
+                } else {
+                    pre = node;
+                    node = node.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int hash(Object key) {
+        if (key == null) {
+            return 0;
+        }
+
+        int h = key.hashCode();
+        h = h ^ (h >>> 16);
+
+        return h;
+    }
+
+    private int calcIndex(int hash) {
+        return  (table.length - 1) & hash;
+    }
+
+    private class Node<K, V> {
+        int hash;
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
         }
     }
-    
-    class SplitCount extends Thread {
-        int from, to, last, result;
-        SumTable table;
-        int[] A;
-    
-        public SplitCount(int from, int to, int last, SumTable table, int[] A) {
-            this.from = from;
-            this.to = to;
-            this.last = last;
-            this.table = table;
-            this.A = A;
-        }
-    
-        @Override
-        public void run() {
-            int sum;
-            int count = 0;
-            for(int i = this.from; i < this.to; ++i) {
-                for(int j = i + 1; j < this.last; ++j) {
-                    sum = A[i] + A[j];
-                    if (sum >= 0) {
-                        count += table.count(-sum, i);
+}
+
+class DataNode {
+    int id;
+    DataNode next;
+    DataNode last;
+
+    public DataNode(int id, DataNode next, DataNode last) {
+        this.id = id;
+        this.next = next;
+        this.last = last;
+    }
+
+    public void put(int id) {
+        DataNode newDataNode = new DataNode(id, null, null);
+        last.next = newDataNode;
+    }
+}
+
+public class HW02_4111056036_4 extends FourSum {
+    @Override
+    public int F_sum(int[] A) {
+        int ans = 0;
+        int n = A.length;
+        java.util.Arrays.sort(A);
+
+        HashMap<Integer, DataNode> table = new HashMap<Integer, DataNode>();
+
+        int sum;
+        DataNode dataNode;
+        for(int i = 0; i < n - 1; ++i) {
+            for(int j = i + 1; j < n; ++j) {
+                sum = A[i] + A[j];
+                if(sum >= 0) {
+                    dataNode = table.get(-sum);
+                    while(dataNode != null) {
+                        if(dataNode.id >= i) {
+                            dataNode = dataNode.next;
+                            continue;
+                        } else {
+                            ans++;
+                        }
+                        dataNode = dataNode.next;
+                    }
+                }
+
+                if(sum <= 0) {
+                    dataNode = table.get(sum);
+                    if(dataNode == null) {
+                        DataNode newDataNode = new DataNode(j, null, null);
+                        newDataNode.last = newDataNode;
+                        table.put(sum, newDataNode);
+                    } else {
+                        DataNode newNode = new DataNode(j, null, null);
+                        dataNode.last.next = newNode;
+                        dataNode.last = newNode;
                     }
                 }
             }
-            this.result = count;
-        }
-    
-        public int getResult() {
-            return this.result;
-        }
-    }
-
-    @Override
-    public int F_sum(int[] A) {
-        int count = 0;
-        int n = A.length;
-
-        SumTable table = new SumTable(500000);
-
-        java.util.Arrays.sort(A);
-
-        int sum;
-        for (int i = 0; i < A.length; ++i) {
-            for (int j = i + 1; j < A.length; ++j) {
-                sum = A[i] + A[j];
-                if (sum <= 0) {
-                    table.put(sum, j);
-                }
-            }
         }
 
-        int threadNums = Runtime.getRuntime().availableProcessors();
-        int amountOnce = n / threadNums;
-
-        SplitCount[] workQueue2 = new SplitCount[threadNums];
-
-        for(int i = 0; i < threadNums - 1; i++) {
-            workQueue2[i] = new SplitCount(amountOnce * i, amountOnce * (i + 1), n, table, A);
-            workQueue2[i].start();
-        }
-        workQueue2[threadNums - 1] = new SplitCount(amountOnce * (threadNums - 1), n - 1, n, table, A);
-        workQueue2[threadNums - 1].start();
-        for(int i = 0; i < threadNums; i++) {
-            try {
-                workQueue2[i].join();
-            } catch (InterruptedException e) { }
-            count += workQueue2[i].getResult();
-        }
-
-        return count;
+        return ans;
     }
 }
