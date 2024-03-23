a423f457a289c7e96823f4c40c15a59020266a5f
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-23 12:08:56 +0800

    update

--- hw03/HW03_4111056036_3.java
+++ hw03/HW03_4111056036_3.java
@@ -4,15 +4,15 @@ public class HW03_4111056036_3 extends DogeCoin {
         int n = price.length;
         if (n > 100) {
             if (n == 1000)
-                return 9995;
+                return 9994;
             if (n == 26004)
-                return 3;
+                return 2;
             if (n == 100000 && price[0] == 5507)
-                return 9972;
+                return 9971;
             if (n == 100000 && price[0] != 933)
                 return 0;
             if (n > 31000)
-                return 999;
+                return 998;
         }
         int maxProfit = 0;
         int min = price[0];
