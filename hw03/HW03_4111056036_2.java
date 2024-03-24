05abf8a4177a5f363c901f9fa06ec540376c2dbe
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-24 20:03:23 +0800

    update hw03 build 2

--- hw03/HW03_4111056036_2.java
+++ hw03/HW03_4111056036_2.java
@@ -1,12 +1,14 @@
 public class HW03_4111056036_2 extends DogeCoin {
     @Override
     public int doge(int[] price) {
-        int[] minPrice = new int[price.length];
-        minPrice[0] = price[0];
         int profit = 0;
-        for(int i = 1; i < price.length; i++){
-            minPrice[i] = Math.min(minPrice[i-1], price[i]);
-            profit = Math.max(profit, price[i] - minPrice[i]);
+        int min = price[0];
+        for(int i = 1; i < price.length; ++i) {
+            if(price[i] < min) {
+                min = price[i];
+            } else if(profit < price[i] - min) {
+                profit = price[i] - min;
+            }
         }
         return profit;
     }
