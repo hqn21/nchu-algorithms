bb8681274e5d9bc54df6ad361d595c0790cc1925
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-25 22:29:42 +0800

    update hw02 build 5

--- hw02/HW02_4111056036_5.java
+++ hw02/HW02_4111056036_5.java
@@ -7,7 +7,7 @@ public class HW02_4111056036_5 extends FourSum {
     public int F_sum(int[] A) {
         int ans = 0;
         int n = A.length;
-        int sums[] = new int[20000];
+        int sums[] = new int[19000];
         java.util.Arrays.sort(A);
         for(int i = 0; i < n - 1; ++i) {
             for(int j = n - 1; j > i; --j) {
