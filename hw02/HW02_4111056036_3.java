8abf0ea0f6afd3b1cec58a895709440f7898b2ef
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-25 17:05:09 +0800

    update hw02 build 3

--- hw02/HW02_4111056036_3.java
+++ hw02/HW02_4111056036_3.java
@@ -3,119 +3,29 @@ public class HW02_4111056036_3 extends FourSum {
         
     }
 
-    public void quickSort(Position[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    private int partition(Position[] arr, int low, int high) {
-        int pivot = arr[high].value;
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j].value < pivot) {
-                i++;
-
-                Position temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        Position temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
-
-    private class Position {
-        int value;
-        int id_1;
-        int id_2;
-
-        Position(int value, int id_1, int id_2) {
-            this.value = value;
-            this.id_1 = id_1;
-            this.id_2 = id_2;
-        }
-    }
-
     @Override
     public int F_sum(int[] A) {
-        int n = A.length;
-        Position[] sums = new Position[n * (n - 1) / 2];
         int ans = 0;
-        
-        int now = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                sums[now] = new Position(A[i] + A[j], i, j);
-                now++;
-            }
-        }
-
-        this.quickSort(sums, 0, sums.length - 1);
-
-        int left = 0;
-        int right = sums.length - 1;
-        int[] record = new int[sums.length];
-        int leftCount, rightCount, low, high, initLeft, initRight;
-        int sum;
-
-        while(left < right) {
-            sum = sums[left].value + sums[right].value;
-            if(sum == 0) {
-                if(sums[left].value == 0) {
-                    for(int i = left; i <= right; i++) {
-                        for(int j = i + 1; j <= right; j++) {
-                            if(sums[i].id_1 != sums[j].id_1 && sums[i].id_1 != sums[j].id_2 && sums[i].id_2 != sums[j].id_1 && sums[i].id_2 != sums[j].id_2) {
-                                ans++;
-                            }
-                        }
-                    }
+        int n = A.length;
+        int sums[] = new int[15000];
+        java.util.Arrays.sort(A);
+        for(int i = 0; i < n - 1; ++i) {
+            for(int j = n - 1; j > i; --j) {
+                if((A[i] + A[j]) < 0) {
                     break;
                 }
+                ans += sums[A[i] + A[j]];
+            }
 
-                leftCount = 0;
-                rightCount = 0;
-                low = sums[left].value;
-                high = sums[right].value;
-
-                initLeft = left;
-                initRight = right;
-
-                while(left < right && sums[left].value == low) {
-                    record[sums[left].id_1]++;
-                    record[sums[left].id_2]++;
-                    leftCount++;
-                    left++;
+            for(int j = 0; j < i; ++j) {
+                if((A[i] + A[j]) > 0) {
+                    break;
                 }
-
-                while(left <= right && sums[right].value == high) {
-                    ans -= record[sums[right].id_1];
-                    ans -= record[sums[right].id_2];
-                    rightCount++;
-                    right--;
-                }
-
-                while(initLeft < initRight && sums[initLeft].value == low) {
-                    record[sums[initLeft].id_1]--;
-                    record[sums[initLeft].id_2]--;
-                    initLeft++;
-                }
-
-                ans += leftCount * rightCount;
-            } else if(sum > 0) {
-                right--;
-            } else {
-                left++;
+                
+                ++sums[(A[i] + A[j]) * (-1)];
             }
         }
 
-        return ans / 3;
+        return ans;
     }
 }
