7480698bec3f29a8d066b2ae50209e9d0d74004a
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-27 22:22:57 +0800

    update hw04 build 1

--- hw04/HW04_4111056036_1.java
+++ hw04/HW04_4111056036_1.java
@@ -1,107 +1,107 @@
-class HashMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 16;
-    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private Node<K, V>[] table;
-    private int size;
-
-    @SuppressWarnings("unchecked")
-    public HashMap() {
-        this.table = new Node[DEFAULT_CAPACITY];
-    }
-
-    @SuppressWarnings("unchecked")
-    private void grew() {
-        if ((size + 1) == (table.length * DEFAULT_LOAD_FACTOR)) {
-            Node<K, V>[] oldTable = table;
-            table = new Node[table.length << 1];
-            size = 0;
-            for (int i = 0; i < oldTable.length; i++) {
-                Node<K, V> node = oldTable[i];
-                while (node != null) {
-                    put(node.key, node.value);
-                    node = node.next;
+public class HW04_4111056036_1 extends LanguageModel {
+    class HashMap<K, V> {
+        private static final int DEFAULT_CAPACITY = 16;
+        private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+        private Node<K, V>[] table;
+        private int size;
+    
+        @SuppressWarnings("unchecked")
+        public HashMap() {
+            this.table = new Node[DEFAULT_CAPACITY];
+        }
+    
+        @SuppressWarnings("unchecked")
+        private void grew() {
+            if ((size + 1) == (table.length * DEFAULT_LOAD_FACTOR)) {
+                Node<K, V>[] oldTable = table;
+                table = new Node[table.length << 1];
+                size = 0;
+                for (int i = 0; i < oldTable.length; i++) {
+                    Node<K, V> node = oldTable[i];
+                    while (node != null) {
+                        put(node.key, node.value);
+                        node = node.next;
+                    }
                 }
             }
         }
-    }
-
-    public void put(K key, V value) {
-        grew();
-
-        int hash = hash(key);
-        int index = calcIndex(hash);
-        Node<K, V> node = table[index];
-        Node<K, V> newNode = new Node<>(key, value, null);
-        if (node == null) {
-            table[index] = newNode;
-        } else {
-            boolean keyRepeat = false;
-            Node<K, V> last = null;
+    
+        public void put(K key, V value) {
+            grew();
+    
+            int hash = hash(key);
+            int index = calcIndex(hash);
+            Node<K, V> node = table[index];
+            Node<K, V> newNode = new Node<>(key, value, null);
+            if (node == null) {
+                table[index] = newNode;
+            } else {
+                boolean keyRepeat = false;
+                Node<K, V> last = null;
+                while (node != null) {
+                    if (node.key.equals(key)) {
+                        keyRepeat = true;
+                        node.value = value;
+                        break;
+                    } else {
+                        last = node;
+                        node = node.next;
+                    }
+                }
+                if (!keyRepeat) {
+                    last.next = newNode;
+                }
+            }
+    
+            size++;
+        }
+    
+        public V get(K key) {
+            int hash = hash(key);
+            int index = calcIndex(hash);
+            Node<K, V> node = table[index];
             while (node != null) {
                 if (node.key.equals(key)) {
-                    keyRepeat = true;
-                    node.value = value;
-                    break;
+                    return node.value;
                 } else {
-                    last = node;
                     node = node.next;
                 }
             }
-            if (!keyRepeat) {
-                last.next = newNode;
+            return null;
+        }
+    
+        public int size() {
+            return size;
+        }
+    
+        private int hash(Object key) {
+            if (key == null) {
+                return 0;
+            }
+    
+            int h = key.hashCode();
+            h = h ^ (h >>> 16);
+    
+            return h;
+        }
+    
+        private int calcIndex(int hash) {
+            return  (table.length - 1) & hash;
+        }
+    
+        private class Node<E, A> {
+            E key;
+            A value;
+            Node<E, A> next;
+    
+            public Node(E key, A value, Node<E, A> next) {
+                this.key = key;
+                this.value = value;
+                this.next = next;
             }
         }
-
-        size++;
     }
 
-    public V get(K key) {
-        int hash = hash(key);
-        int index = calcIndex(hash);
-        Node<K, V> node = table[index];
-        while (node != null) {
-            if (node.key.equals(key)) {
-                return node.value;
-            } else {
-                node = node.next;
-            }
-        }
-        return null;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int hash(Object key) {
-        if (key == null) {
-            return 0;
-        }
-
-        int h = key.hashCode();
-        h = h ^ (h >>> 16);
-
-        return h;
-    }
-
-    private int calcIndex(int hash) {
-        return  (table.length - 1) & hash;
-    }
-
-    private class Node<E, A> {
-        E key;
-        A value;
-        Node<E, A> next;
-
-        public Node(E key, A value, Node<E, A> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-}
-
-public class HW04_4111056036_1 extends LanguageModel {
     @Override
     public String nextPredictToken(String[] A) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
