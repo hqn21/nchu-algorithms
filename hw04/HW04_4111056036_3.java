541866eb1f31eacf092ede443e599cde337cf11b
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-03-31 12:20:16 +0800

    update hw04 build 3 5

--- hw04/HW04_4111056036_3.java
+++ hw04/HW04_4111056036_3.java
@@ -109,26 +109,55 @@ public class HW04_4111056036_3 extends LanguageModel {
 
     @Override
     public String nextPredictToken(String[] A) {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> record = new HashMap<>();
+        int targetLength = A[0].length();
+        
+        int nowChecking = 0;
+        int now = 0;
+        int n = A[1].length();
         int max = 0;
-        String ans = "";
-        String[] words = A[1].split(" ");
+        int prev = 0;
+        int ansStart = 0, ansEnd = 0;
         boolean insert = false;
 
-        for (String word : words) {
-            if (word.equals(A[0])) {
-                insert = true;
-            } else if (insert) {
-                int count = map.getOrDefault(word, 0) + 1;
-                map.put(word, count);
-                if (count > max) {
-                    ans = word;
-                    max = count;
+        for(int i = 0; i < n; ++i) {
+            if(A[1].charAt(i) == ' ') {
+                if(insert) {
+                    record.put(now, record.getOrDefault(now, 0) + 1);
+                    if(record.get(now) > max) {
+                        max = record.get(now);
+                        ansStart = prev;
+                        ansEnd = i;
+                    }
+                    now = 0;
+                    insert = false;
+                } else {
+                    insert = true;
+                    prev = i + 1;
                 }
-                insert = false;
+            } else if(insert) {
+                now += (int) A[1].charAt(i);
+            } else {
+                if(nowChecking >= targetLength || (A[1].charAt(i) != A[0].charAt(nowChecking))) {
+                    while(i < n && A[1].charAt(i) != ' ') {
+                        ++i;
+                    }
+                    nowChecking = 0;
+                } else {
+                    ++nowChecking;
+                }
             }
         }
 
-        return ans;
+        if(insert) {
+            record.put(now, record.getOrDefault(now, 0) + 1);
+            if(record.get(now) > max) {
+                max = record.get(now);
+                ansStart = prev;
+                ansEnd = n;
+            }
+        }
+
+        return A[1].substring(ansStart, ansEnd);
     }
 }
