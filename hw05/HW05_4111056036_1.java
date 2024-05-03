3b77825fd88fa2e4523a0f63fcc415d6b1612db0
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-05-03 20:57:55 +0800

    update hw05 build 1

--- hw05/HW05_4111056036_1.java
+++ hw05/HW05_4111056036_1.java
@@ -2,79 +2,75 @@ public class HW05_4111056036_1 extends WordChain {
     @Override
     public String sequenceProbability(String[] A) {
         int firstStart = 0, firstEnd = 0, secondStart = 0, secondEnd = 0, thirdStart = 0, thirdEnd = 0;
-        String first, second, third;
-        String target = A[0] + " ";
-        int targetLength = target.length();
-        int start = A[1].indexOf(target);
-        int end;
-        int[] record = new int[50];
+        int n = A[1].length();
+        int[] record = new int[100];
+        boolean insert = false;
+        int temp = 0;
+        int t;
         int max = 0;
-        int dataId = 0;
-        while(start != -1) {
-            start += targetLength;
-            end = A[1].indexOf(" ", start + 1);
-            if(end != -1) {
-                for(int i = start; i <= end; ++i) {
-                    dataId += A[1].charAt(i);
+        int i;
+        for(i = 0; i < n; i += 3) {
+            if(insert) {
+                temp = (A[1].charAt(i) + A[1].charAt(i + 1)) % 100;
+                if(++record[temp] > max) {
+                    max = record[temp];
+                    firstStart = i;
+                    firstEnd = i + 2;
                 }
-                dataId %= 50;
-                if(++record[dataId] > max) {
-                    max = record[dataId];
-                    firstStart = start;
-                    firstEnd = end;
-                }
-                dataId = 0;
+                insert = false;
+                temp = 0;
+                continue;
             }
-            start = A[1].indexOf(target, start + 1);
+            if(A[1].charAt(i) != A[0].charAt(0) || A[1].charAt(i + 1) != A[0].charAt(1)) {
+                continue;
+            }
+            insert = true;
         }
-        first = A[1].substring(firstStart, firstEnd) + " ";
 
-        targetLength = first.length();
-        start = A[1].indexOf(first);
-        java.util.Arrays.fill(record, 0);
+        t = firstStart + 1;
+        insert = false;
         max = 0;
-        while(start != -1) {
-            start += targetLength;
-            end = A[1].indexOf(" ", start + 1);
-            if(end != -1) {
-                for(int i = start; i <= end; ++i) {
-                    dataId += A[1].charAt(i);
-                }
-                dataId %= 50;
-                if(++record[dataId] > max) {
-                    max = record[dataId];
-                    secondStart = start;
-                    secondEnd = end;
-                }
-                dataId = 0;
-            }
-            start = A[1].indexOf(first, start + 1);
-        }
-        second = A[1].substring(secondStart, secondEnd) + " ";
-
-        targetLength = second.length();
-        start = A[1].indexOf(second);
         java.util.Arrays.fill(record, 0);
-        max = 0;
-        while(start != -1) {
-            start += targetLength;
-            end = A[1].indexOf(" ", start + 1);
-            if(end != -1) {
-                for(int i = start; i <= end; ++i) {
-                    dataId += A[1].charAt(i);
+        for(i = 0; i < n; i += 3) {
+            if(insert) {
+                temp = (A[1].charAt(i) + A[1].charAt(i + 1)) % 100;
+                if(++record[temp] > max) {
+                    max = record[temp];
+                    secondStart = i;
+                    secondEnd = i + 2;
                 }
-                dataId %= 50;
-                if(++record[dataId] > max) {
-                    max = record[dataId];
-                    thirdStart = start;
-                    thirdEnd = end;
-                }
-                dataId = 0;
+                insert = false;
+                temp = 0;
+                continue;
             }
-            start = A[1].indexOf(second, start + 1);
+            if(A[1].charAt(i) != A[1].charAt(firstStart) || A[1].charAt(i + 1) != A[1].charAt(t)) {
+                continue;
+            }
+            insert = true;
         }
-        third = A[1].substring(thirdStart, thirdEnd);
 
-        return target + first + second + third;
+        t = secondStart + 1;
+        insert = false;
+        max = 0;
+        java.util.Arrays.fill(record, 0);
+        for(i = 0; i < n; i += 3) {
+            if(insert) {
+                temp = (A[1].charAt(i) + A[1].charAt(i + 1)) % 100;
+                if(++record[temp] > max) {
+                    max = record[temp];
+                    thirdStart = i;
+                    thirdEnd = i + 2;
+                }
+                insert = false;
+                temp = 0;
+                continue;
+            }
+            if(A[1].charAt(i) != A[1].charAt(secondStart) || A[1].charAt(i + 1) != A[1].charAt(t)) {
+                continue;
+            }
+            insert = true;
+        }
+
+        return A[0] + " " + A[1].substring(firstStart, firstEnd) + " " + A[1].substring(secondStart, secondEnd) + " " + A[1].substring(thirdStart, thirdEnd);
     }
 }
