b417a474c933e1ba9db296428d7a9977be1e8e47
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-05-04 22:01:28 +0800

    update hw06

--- hw06/HW06.java
+++ hw06/HW06.java
@@ -1,10 +1,27 @@
 import java.io.*;
 
 public class HW06 {
-    public static void test() {
+    static class Result {
+        Double[] ans;
+        double time;
+        boolean correct;
+
+        public Result(Double[] ans, double time, boolean correct) {
+            this.ans = ans;
+            this.time = time;
+            this.correct = correct;
+        }
+    }
+
+    public static Result test(SortIsAllYouNeed s, int id) {
+        int[] lines = {1000, 1000, 1000, 1000, 1000, 1000, 1000, 2001, 1000};
+        Double[] A = new Double[lines[id - 1]];
+        int now = 0;
+
+        // read file
         FileReader fr = null;
         try {
-            fr = new FileReader("./HW6_test_data/answer_1.txt");
+            fr = new FileReader("./HW6_test_data/test_data_" + id + ".txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -13,7 +30,8 @@ public class HW06 {
 
         try {
             while (((tmp = br.readLine()) != null)) {
-                System.out.println(Double.parseDouble(tmp));
+                A[now] = Double.parseDouble(tmp);
+                ++now;
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,9 +42,71 @@ public class HW06 {
                 e.printStackTrace();
             }
         }
+
+        // start test
+        StopWatch stopWatch = new StopWatch();
+        s.sortWhat(A);
+        double time = stopWatch.elapsedTime();
+        boolean correct = checkAnswer(A, id);
+        Result result = new Result(A, time, correct);
+        return result;
+    }
+
+
+    public static boolean checkAnswer(Double[] ans, int id) {
+        int now = 0;
+        FileReader fr = null;
+        try {
+            fr = new FileReader("./HW6_test_data/answer_" + id + ".txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(fr);
+        String tmp = null;
+
+        try {
+            while (((tmp = br.readLine()) != null)) {
+                if(ans[now] != Double.parseDouble(tmp)) {
+                    return false;
+                }
+                ++now;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true;
+    }
+
+    public static void printArray(Double[] A) {
+        System.out.print("{");
+        System.out.print(A[0]);
+        for(int i = 1; i < A.length; ++i) {
+            System.out.print(", " + A[i]);
+        }
+        System.out.println("}");
     }
 
     public static void main(String[] args) {
-        test();
+        HW06_4111056036_1 build1 = new HW06_4111056036_1();
+        HW06_4111056036_2 build2 = new HW06_4111056036_2();
+        Result result;
+
+        for(int i = 1; i <= 9; ++i) {
+            System.out.println("=====[TEST " + i + "]=====");
+            result = test(build1, i);
+            System.out.println("Build 1: " + result.time + "s [" + (result.correct ? "CORRECT" : "WRONG") + "]");
+            // printArray(result.ans);
+            result = test(build2, i);
+            System.out.println("Build 2: " + result.time + "s [" + (result.correct ? "CORRECT" : "WRONG") + "]");
+            // printArray(result.ans);
+            System.out.println("==================");
+        }
     }
 }
