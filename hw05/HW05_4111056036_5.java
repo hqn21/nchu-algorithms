aedfeae05268a76987c29e4febf2b91fb701c8dc
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-05-08 02:58:17 +0800

    update hw05 build 5

--- hw05/HW05_4111056036_5.java
+++ hw05/HW05_4111056036_5.java
@@ -1,212 +1,82 @@
 public class HW05_4111056036_5 extends WordChain {
-    class HashMap<K, V> {
-        private static final int DEFAULT_CAPACITY = 16;
-        private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-        private Node<K, V>[] table;
-        private int size;
-    
-        @SuppressWarnings("unchecked")
-        public HashMap() {
-            this.table = new Node[DEFAULT_CAPACITY];
-        }
-    
-        @SuppressWarnings("unchecked")
-        private void grew() {
-            if ((size + 1) == (table.length * DEFAULT_LOAD_FACTOR)) {
-                Node<K, V>[] oldTable = table;
-                table = new Node[table.length << 1];
-                size = 0;
-                for (int i = 0; i < oldTable.length; ++i) {
-                    Node<K, V> node = oldTable[i];
-                    while (node != null) {
-                        put(node.key, node.value);
-                        node = node.next;
-                    }
-                }
-            }
-        }
-    
-        public void put(K key, V value) {
-            grew();
-    
-            int hash = hash(key);
-            int index = calcIndex(hash);
-            Node<K, V> node = table[index];
-            Node<K, V> newNode = new Node<>(key, value, null);
-            if (node == null) {
-                table[index] = newNode;
-            } else {
-                boolean keyRepeat = false;
-                Node<K, V> last = null;
-                while (node != null) {
-                    if (node.key.equals(key)) {
-                        keyRepeat = true;
-                        node.value = value;
-                        break;
-                    } else {
-                        last = node;
-                        node = node.next;
-                    }
-                }
-                if (!keyRepeat) {
-                    last.next = newNode;
-                }
-            }
-    
-            ++size;
-        }
-    
-        public V get(K key) {
-            int hash = hash(key);
-            int index = calcIndex(hash);
-            Node<K, V> node = table[index];
-            while (node != null) {
-                if (node.key.equals(key)) {
-                    return node.value;
-                } else {
-                    node = node.next;
-                }
-            }
-            return null;
-        }
-    
-        public int size() {
-            return size;
-        }
-    
-        private int hash(Object key) {
-            if (key == null) {
-                return 0;
-            }
-    
-            int h = key.hashCode();
-            h = h ^ (h >>> 16);
-    
-            return h;
-        }
-    
-        private int calcIndex(int hash) {
-            return  (table.length - 1) & hash;
+    static class Vertex {
+        String word;
+        double count;
+
+        Vertex next;
+
+        Vertex(String word, double count) {
+            this.word = word;
+            this.count = count;
         }
 
-        public V getOrDefault(K key, V defaultValue) {
-            V value = get(key);
-            return (value != null) ? value : defaultValue;
-        }
-    
-        private class Node<E, A> {
-            E key;
-            A value;
-            Node<E, A> next;
-    
-            public Node(E key, A value, Node<E, A> next) {
-                this.key = key;
-                this.value = value;
-                this.next = next;
+        public void connect(String word) {
+            count += 1;
+
+            Vertex current = next;
+            while (current != null) {
+                if (current.word.equals(word)) {
+                    current.count += 1;
+                    return;
+                }
+                current = current.next;
             }
+
+            Vertex newVertex = new Vertex(word, 1);
+
+            newVertex.next = next;
+            next = newVertex;
         }
     }
 
-    private static class Position {
-        int start;
-        int end;
-        float probability;
+    Vertex[] vertices = new Vertex[512];
 
-        public Position(int start, int end, float probability) {
-            this.start = start;
-            this.end = end;
-            this.probability = probability;
+    String A1, A0;
+    int A1Len;
+
+    String[] path = new String[4];
+    double maxProb = 0;
+
+    public void dfs(String current, double prob, int k) {
+        if (k == 0) {
+            if (prob > maxProb) {
+                maxProb = prob;
+            }
+
+            return;
         }
-    }
 
-    private static Position greedyFind(String target, String A) {
-        int[] record = new int[128];
-        int start = A.indexOf(target);
-        int end;
-        int targetLength = target.length();
-        int dataId = 0;
-        int max = 0;
-        int amount = 0;
-        Position position = new Position(0, 0, 0);
+        Vertex srcVertex = vertices[current.hashCode() & 511];
 
-        while(start != -1) {
-            ++amount;
-            start += targetLength;
-            end = A.indexOf(" ", start + 1);
-            if(end == -1) {
-                break;
+        if (srcVertex == null) {
+            srcVertex = vertices[current.hashCode() & 511] = new Vertex(current, 0);
+
+            for (int i = A1.indexOf(current); i != -1 && i + 5 <= A1Len; i = A1.indexOf(current, i + 1)) {
+                srcVertex.connect(A1.substring(i + 3, i + 5));
             }
-            for(int i = start; i <= end; ++i) {
-                dataId += A.charAt(i);
-            }
-            dataId &= 127;
-            if(++record[dataId] > max) {
-                max = record[dataId];
-                position.start = start;
-                position.end = end;
-            }
-            dataId = 0;
-            start = A.indexOf(target, start);
         }
-        position.probability = (float) max / amount;
-        return position;
-    }
 
-    private static void simpleFind(StringBuilder sb, String A, String[] targets, int targetAmount, float probability, float maxProbability, int level) {
+        for (Vertex next = srcVertex.next; next != null; next = next.next) {
+            double nextProb = prob * next.count / srcVertex.count;
+            if (nextProb <= maxProb) {
+                continue;
+            }
 
+            path[4 - k] = next.word;
+            dfs(next.word, prob * next.count / srcVertex.count, k - 1);
+        }
     }
 
     @Override
     public String sequenceProbability(String[] A) {
-        StringBuilder sb = new StringBuilder();
-        String target = A[0] + " ", first, second;
-        int targetLength = target.length();
+        A0 = A[0];
 
-        int[] record = new int[128];
-        int start = A[1].indexOf(target);
-        int end;
-        int dataId = 0;
-        int max = 0;
-        int amount = 0;
-        Position position = new Position(0, 0, 0);
-        Position[] targets = new Position[100];
-        HashMap<Integer, Integer> targetDatas = new HashMap<Integer, Integer>();
-        float[] probabilities = new float[100];
+        A1 = " " + A[1];
+        A1Len = A1.length();
 
-        while(start != -1) {
-            start += targetLength;
-            end = A[1].indexOf(" ", start + 1);
-            if(end == -1) {
-                break;
-            }
-            // targets[amount] = 
-            // ++amount;
-            for(int i = start; i <= end; ++i) {
-                dataId += A[1].charAt(i);
-            }
-            dataId &= 127;
-            if(++record[dataId] > max) {
-                max = record[dataId];
-                position.start = start;
-                position.end = end;
-            }
-            dataId = 0;
-            start = A[1].indexOf(target, start);
-        }
-        position.probability = (float) max / amount;
+        path[0] = A[0];
+        dfs(A[0], 1, 3);
 
-        float maxProbability = position.probability;
-        first = A[1].substring(position.start, position.end) + " ";
-        position = greedyFind(first, A[1]);
-        maxProbability *= position.probability;
-        second = A[1].substring(position.start, position.end) + " ";
-        position = greedyFind(second, A[1]);
-        maxProbability *= position.probability;
-        sb.append(target);
-        sb.append(first);
-        sb.append(second);
-        sb.append(A[1].substring(position.start, position.end));
-        // simpleFind(sb, A[1], targets, amount, maxProbability, 1, 1);
-        return sb.toString();
+        return String.join(" ", path);
     }
 }
