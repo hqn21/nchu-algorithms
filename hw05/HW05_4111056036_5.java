fc3e6eb337b52c5c822e32ed084f75c547d3e9ea
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-05-08 03:05:53 +0800

    update hw05 build 5

--- hw05/HW05_4111056036_5.java
+++ hw05/HW05_4111056036_5.java
@@ -1,81 +1,190 @@
 public class HW05_4111056036_5 extends WordChain {
-    static class Vertex {
-        String word;
-        double count;
-
-        Vertex next;
-
-        Vertex(String word, double count) {
-            this.word = word;
-            this.count = count;
+    class HashMap<K, V> {
+        private static final int DEFAULT_CAPACITY = 16;
+        private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+        private Node<K, V>[] table;
+        private int size;
+    
+        @SuppressWarnings("unchecked")
+        public HashMap() {
+            this.table = new Node[DEFAULT_CAPACITY];
+        }
+    
+        @SuppressWarnings("unchecked")
+        private void grew() {
+            if ((size + 1) == (table.length * DEFAULT_LOAD_FACTOR)) {
+                Node<K, V>[] oldTable = table;
+                table = new Node[table.length << 1];
+                size = 0;
+                for (int i = 0; i < oldTable.length; ++i) {
+                    Node<K, V> node = oldTable[i];
+                    while (node != null) {
+                        put(node.key, node.value);
+                        node = node.next;
+                    }
+                }
+            }
+        }
+    
+        public void put(K key, V value) {
+            grew();
+    
+            int hash = hash(key);
+            int index = calcIndex(hash);
+            Node<K, V> node = table[index];
+            Node<K, V> newNode = new Node<>(key, value, null);
+            if (node == null) {
+                table[index] = newNode;
+            } else {
+                boolean keyRepeat = false;
+                Node<K, V> last = null;
+                while (node != null) {
+                    if (node.key.equals(key)) {
+                        keyRepeat = true;
+                        node.value = value;
+                        break;
+                    } else {
+                        last = node;
+                        node = node.next;
+                    }
+                }
+                if (!keyRepeat) {
+                    last.next = newNode;
+                }
+            }
+    
+            ++size;
+        }
+    
+        public V get(K key) {
+            int hash = hash(key);
+            int index = calcIndex(hash);
+            Node<K, V> node = table[index];
+            while (node != null) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    
+        public int size() {
+            return size;
+        }
+    
+        private int hash(Object key) {
+            if (key == null) {
+                return 0;
+            }
+    
+            int h = key.hashCode();
+            h = h ^ (h >>> 16);
+    
+            return h;
+        }
+    
+        private int calcIndex(int hash) {
+            return  (table.length - 1) & hash;
         }
 
-        public void connect(String word) {
-            count += 1;
-
-            Vertex current = next;
-            while (current != null) {
-                if (current.word.equals(word)) {
-                    current.count += 1;
-                    return;
-                }
-                current = current.next;
+        public V getOrDefault(K key, V defaultValue) {
+            V value = get(key);
+            return (value != null) ? value : defaultValue;
+        }
+    
+        private class Node<E, A> {
+            E key;
+            A value;
+            Node<E, A> next;
+    
+            public Node(E key, A value, Node<E, A> next) {
+                this.key = key;
+                this.value = value;
+                this.next = next;
             }
-
-            Vertex newVertex = new Vertex(word, 1);
-
-            newVertex.next = next;
-            next = newVertex;
         }
     }
 
-    Vertex[] vertices = new Vertex[512];
+    static class Node {
+        String word;
+        double frequency;
 
-    String A1, A0;
-    int A1Len;
+        Node next;
+
+        Node(String word, double frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+ 
+        public void connect(String word) {
+            frequency += 1;
+
+            Node currentNode = next;
+            while (currentNode != null) {
+                if (currentNode.word.equals(word)) {
+                    currentNode.frequency += 1;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+
+            Node newNode = new Node(word, 1);
+
+            newNode.next = next;
+            next = newNode;
+        }
+    }
+
+    HashMap<String, Node> nodes = new HashMap<>();
+
+    String sequence, initialSequence;
+    int sequenceLength;
 
     String[] path = new String[4];
-    double maxProb = 0;
+    double maxProbability = 0;
 
-    public void dfs(String current, double prob, int k) {
-        if (k == 0) {
-            if (prob > maxProb) {
-                maxProb = prob;
+    public void depthFirstSearch(String current, double probability, int depth) {
+        if (depth == 0) {
+            if (probability > maxProbability) {
+                maxProbability = probability;
             }
 
             return;
         }
 
-        Vertex srcVertex = vertices[current.hashCode() & 511];
+        Node sourceNode = nodes.get(current);
 
-        if (srcVertex == null) {
-            srcVertex = vertices[current.hashCode() & 511] = new Vertex(current, 0);
+        if (sourceNode == null) {
+            sourceNode = new Node(current, 0);
+            nodes.put(current, sourceNode);
 
-            for (int i = A1.indexOf(current); i != -1 && i + 5 <= A1Len; i = A1.indexOf(current, i + 1)) {
-                srcVertex.connect(A1.substring(i + 3, i + 5));
+            for (int i = sequence.indexOf(current); i != -1 && i + 5 <= sequenceLength; i = sequence.indexOf(current, i + 1)) {
+                sourceNode.connect(sequence.substring(i + 3, i + 5));
             }
         }
 
-        for (Vertex next = srcVertex.next; next != null; next = next.next) {
-            double nextProb = prob * next.count / srcVertex.count;
-            if (nextProb <= maxProb) {
+        for (Node next = sourceNode.next; next != null; next = next.next) {
+            double nextProbability = probability * next.frequency / sourceNode.frequency;
+            if (nextProbability <= maxProbability) {
                 continue;
             }
 
-            path[4 - k] = next.word;
-            dfs(next.word, prob * next.count / srcVertex.count, k - 1);
+            path[4 - depth] = next.word;
+            depthFirstSearch(next.word, nextProbability, depth - 1);
         }
     }
 
     @Override
     public String sequenceProbability(String[] A) {
-        A0 = A[0];
+        initialSequence = A[0];
 
-        A1 = " " + A[1];
-        A1Len = A1.length();
+        sequence = " " + A[1];
+        sequenceLength = sequence.length();
 
-        path[0] = A[0];
-        dfs(A[0], 1, 3);
+        path[0] = initialSequence;
+        depthFirstSearch(initialSequence, 1, 3);
 
         return String.join(" ", path);
     }
