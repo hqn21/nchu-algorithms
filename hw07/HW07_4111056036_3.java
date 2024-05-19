a000d8e6119d282f9717aaff2ae825b1c7398d38
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2024-05-19 23:31:09 +0800

    update hw07 build 3 4

--- hw07/HW07_4111056036_3.java
+++ hw07/HW07_4111056036_3.java
@@ -188,7 +188,11 @@ public class HW07_4111056036_3 extends LSD {
         marked = new boolean[n];
         distTo = new int[n];
 
-        bfs(graph, (int) Math.random() * n);
+        int farthestNode = bfs(graph, 0);
+
+        marked = new boolean[n];
+        
+        bfs(graph, farthestNode);
 
         return maxDist;
     }
